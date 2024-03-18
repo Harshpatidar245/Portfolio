2da73fd10a51ec176cdd9cca546f32bea95a432c
harshpatidar245 <harshpatidar2400@gmail.com>
2024-03-18 22:22:17 +0530

    first commit

---
+++ src/Pages/Home/AboutMe.jsx
@@ -0,0 +1,27 @@
+export default function AboutMe() {
+  return (
+    <section id="AboutMe" className="about--section">
+      <div className="about--section--img">
+        <img src="./img/harsh image.jpg" alt="aboutMe" />
+      </div>
+      <div
+        className="hero--section--content--box
+        about--section--box"
+      >
+        <div className="hero--section-content">
+          <p className="section-title"></p>
+          <h1 className="skills-secton--heading">About Me</h1>
+          <p className="hero--section-description">
+          Hello, My name is Harsh Patidar Currently in 3rd year, pursuing B.tech in Information Technology from Acropolis Institute Of Technology And Research Indore 
+          </p>
+          <p className="hero--section-description">
+            {/* Lorem ipsum dolor sit amet consectetur adipisicing elit. Officia
+            vitae ex nihil cum corporis! Facilis sit nam perferendis ratione
+            dicta accusantium nihil, temporibus tempora, architecto, corrupti
+            rerum esse? Eveniet, sed. */}
+          </p>
+        </div>
+      </div>
+    </section>
+  );
+}
